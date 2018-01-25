294c959894ac8e1689347ebecb65acba66ceb388
neilsethi13 <34347691+neilsethi13@users.noreply.github.com>
2018-01-24 22:05:44 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import BusinessList from '.../Components/BusinessList/BusinessList';
+import SearchBar from '.../Components/SearchBar/SearchBar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
+}
+
+export default App;
